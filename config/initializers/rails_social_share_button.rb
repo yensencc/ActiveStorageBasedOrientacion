8540fe302f3e49966525ba5e00733a5fd8efb764
Ubuntu <ubuntu@ip-172-31-87-217.ec2.internal>
2020-04-11 19:24:26 +0000

    fixing social share

--- config/initializers/rails_social_share_button.rb
+++ config/initializers/rails_social_share_button.rb
@@ -1,5 +1,5 @@
 RailsSocialShareButton.configure do |config|
-  config.allow_sites = %w(twitter facebook weibo whatsapp_app whatsapp_web email)
+  config.allow_sites = %w(twitter linkedin facebook weibo whatsapp_app whatsapp_web email google_bookmark pinterest)
 end
 
 
