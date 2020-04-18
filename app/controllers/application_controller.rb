c65a3ad4daf1bdfbc5a74974e638fb001fd5545f
yensencc <yensencc@gmail.com>
2020-04-17 20:33:03 -0400

    seting up metadata initiliazers and configurations for robust metedata

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
 def original_url
   original_fullpath
 end
+
+def default_url_options
+  { host: ENV["DOMAIN"] || "localhost:3000" }
+end
     
 protected
 
